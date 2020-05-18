590c1e2716b0ac2be02a12c990c9482c9ace7185
VincentCCandela <vincent.candela14@gmail.com>
2020-05-17 20:37:11 -0400

    nothing useful yet

---
+++ intro_to_pointers.c
@@ -0,0 +1,33 @@
+//#include <stdio.h>
+#include <stdlib.h>
+
+int checker(){
+
+}
+
+adder(){
+
+}
+
+int main (void) {
+    printf("What's poppin? \n");
+    
+    int x,y;
+    int *px; 
+    
+    x = 7;
+
+    px = &x;
+    printf("The address of x is: %p\n" , px);
+    printf("The value of x is: %d\n" , *px);
+
+    x++;
+
+    printf("%d\n" , *px);
+
+    if(*px % 3 != 0){
+        printf("oops, x is not divisible by 3! \n");
+
+    }
+
+}
