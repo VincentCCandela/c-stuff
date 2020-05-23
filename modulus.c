1be89adfa48efb36bcd824187cc0a3e1f8a44ddb
VincentCCandela <vincent.candela14@gmail.com>
2020-05-23 13:49:51 -0400

    moduls stuff

---
+++ modulus.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void checker();
+void adder();
+
+int main(){
+    int a,b,c,d;
+
+    printf("input a number: ");
+    scanf("%d",&a);
+
+    printf("input an integer divisor: ");
+    scanf("%d",&b);
+
+    c = a % b;
+
+    if (c != 0){
+        if(c<0){
+            c += b;
+        }
+        printf("The number does not divide evenly. The remainder is: %d \n",c);
+    }
+    else{
+        printf("The number divides evenly. \n");
+    }
+
+    d = a;
+
+    while(d % b != 0){
+        d++;
+    }
+
+    printf("The next number divisible by %d is %d \n", b, d);
+    return 0;
+}
